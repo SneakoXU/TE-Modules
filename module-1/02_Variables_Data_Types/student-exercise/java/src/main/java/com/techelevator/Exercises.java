4d055bc77b5e802317df0d81e7c56e887c1ab716
Niko Suntay <suntay.3@gmail.com>
2020-09-15 22:26:53 -0400

    exercise problems 1-50

--- module-1/02_Variables_Data_Types/student-exercise/java/src/main/java/com/techelevator/Exercises.java
+++ module-1/02_Variables_Data_Types/student-exercise/java/src/main/java/com/techelevator/Exercises.java
@@ -277,7 +277,9 @@ public class Exercises {
 		
 		double moneyInPocket = .50D;
 		double costOfPencil = .07D;
-		double pencilsCanBuy = moneyInPocket/costOfPencil;
+		double pencilsCanBuyDec = moneyInPocket/costOfPencil;
+		int pencilsCanBuy = (int) pencilsCanBuyDec;
+		//System.out.format("%.2f%n", pencilsCanBuy);
 		System.out.println(pencilsCanBuy);
 		
 
@@ -316,88 +318,175 @@ public class Exercises {
         31. Joy will see her grandma in two days. How many hours until she sees
         her?
         */
+		
+		int daysUntilGrandma = 2;
+		int hoursInADay = 24;
+		int hoursUntilGrandma = daysUntilGrandma * hoursInADay;
+		System.out.println(hoursUntilGrandma);
 
         /*
         32. Kim has 4 cousins. She wants to give each one 5 pieces of gum. How
         much gum will she need?
         */
+		
+		int numberOfCousins = 4;
+		int piecesOfGum = 5;
+		int totalGumNeeded = numberOfCousins * piecesOfGum;
+		System.out.println(totalGumNeeded);
 
         /*
         33. Dan has $3.00. He bought a candy bar for $1.00. How much money is
         left?
         */
+		
+		double danMoney = 3.00;
+		double priceCandyBar = 1.00;
+		double moneyRemaining = danMoney - priceCandyBar;
+		System.out.format("$%.2f%n", moneyRemaining);
+		
+		
 
         /*
         34. 5 boats are in the lake. Each boat has 3 people. How many people are
         on boats in the lake?
         */
+		
+		int boatsOnLake = 5;
+		int peopleOnBoat = 3;
+		int totalPeople = boatsOnLake * peopleOnBoat;
+		System.out.println(totalPeople);
+		
 
         /*
         35. Ellen had 380 legos, but she lost 57 of them. How many legos does she
         have now?
         */
 
+		int ellenLegos = 380;
+		int legosLost = 57;
+		int legosLeft = ellenLegos - legosLost;
+		System.out.println(legosLeft);
+		
         /*
         36. Arthur baked 35 muffins. How many more muffins does Arthur have to
         bake to have 83 muffins?
         */
+		
+		int muffinsArthur = 35;
+		int muffinsGoal = 83;
+		int muffinsTo83 = muffinsGoal - muffinsArthur;
+		System.out.println(muffinsTo83);
+		
 
         /*
         37. Willy has 1400 crayons. Lucy has 290 crayons. How many more
         crayons does Willy have then Lucy?
         */
+		
+		int crayonsWilly = 1400;
+		int crayonsLucy = 290;
+		int diffCrayonsWillyLucy = crayonsWilly - crayonsLucy;
+		System.out.println(diffCrayonsWillyLucy);
+		
 
         /*
         38. There are 10 stickers on a page. If you have 22 pages of stickers, how
         many stickers do you have?
         */
+		
+		int stickersPerPage = 10;
+		int pagesOfStickers = 22;
+		int totalStickers = stickersPerPage * pagesOfStickers;
+		System.out.println(totalStickers);
 
         /*
         39. There are 96 cupcakes for 8 children to share. How much will each
         person get if they share the cupcakes equally?
         */
+		
+		int totalCupcakes = 96;
+		int numberOfChildren = 8;
+		int cupcakesPerChild = totalCupcakes / numberOfChildren;
+		System.out.println(cupcakesPerChild);
+		
 
         /*
         40. She made 47 gingerbread cookies which she will distribute equally in
         tiny glass jars. If each jar is to contain six cookies each, how many
         cookies will not be placed in a jar?
         */
+		
+		int madeCookies = 47;
+		int cookiesPerJar = 6;
+		int cookiesNotInJar = 47%6;
+		System.out.println(cookiesNotInJar);
 
         /*
         41. She also prepared 59 croissants which she plans to give to her 8
         neighbors. If each neighbor received and equal number of croissants,
         how many will be left with Marian?
         */
+		
+		int croissants = 59;
+		int numberOfNeighbors = 8;
+		int croissantsLeft = croissants % numberOfNeighbors;
+		System.out.println(croissantsLeft);
 
         /*
         42. Marian also baked oatmeal cookies for her classmates. If she can
         place 12 cookies on a tray at a time, how many trays will she need to
         prepare 276 oatmeal cookies at a time?
         */
+		
+		int cookiesPerTray = 12;
+		int totalCookies = 276;
+		int numberOfTraysNeeded = totalCookies/cookiesPerTray;
+		System.out.println(numberOfTraysNeeded);
 
         /*
         43. Marian’s friends were coming over that afternoon so she made 480
         bite-sized pretzels. If one serving is equal to 12 pretzels, how many
         servings of bite-sized pretzels was Marian able to prepare?
         */
+		
+		int totalPretzels = 480;
+		int servingPretzels = 12;
+		int totalServingsMade = totalPretzels/servingPretzels;
+		System.out.println(totalServingsMade);
 
         /*
         44. Lastly, she baked 53 lemon cupcakes for the children living in the city
         orphanage. If two lemon cupcakes were left at home, how many
         boxes with 3 lemon cupcakes each were given away?
         */
+		
+		int lemonCupcakes = 53;
+		lemonCupcakes -= 2;
+		int cupcakesPerBox = 3;
+		int numberOfBoxes = lemonCupcakes/cupcakesPerBox;
+		System.out.println(numberOfBoxes);
 
         /*
         45. Susie's mom prepared 74 carrot sticks for breakfast. If the carrots
         were served equally to 12 people, how many carrot sticks were left
         uneaten?
         */
+		
+		int totalCarrots = 74;
+		int numberOfPeople = 12;
+		int carrotsUneaten = 74%12;
+		
 
         /*
         46. Susie and her sister gathered all 98 of their teddy bears and placed
         them on the shelves in their bedroom. If every shelf can carry a
         maximum of 7 teddy bears, how many shelves will be filled?
         */
+		
+		int totalTeddyBears = 98;
+		int teddyBearsPerShelf = 7;
+		int totalShelvesFilled = totalTeddyBears / teddyBearsPerShelf;
+		System.out.println(totalShelvesFilled);
 
         /*
         47. Susie’s mother collected all family pictures and wanted to place all of
@@ -405,23 +494,44 @@ public class Exercises {
         albums will she need if there are 480 pictures?
         */
 
+		int picturesPerAlbum = 20;
+		int totalPictures = 480;
+		int albumsNeeded = totalPictures / picturesPerAlbum;
+		System.out.println(albumsNeeded);
+		
         /*
         48. Joe, Susie’s brother, collected all 94 trading cards scattered in his
         room and placed them in boxes. If a full box can hold a maximum of 8
         cards, how many boxes were filled and how many cards are there in
         the unfilled box?
         */
+		
+		int tradingCards = 94;
+		int cardsPerBox = 8;
+		int filledBoxes = tradingCards/cardsPerBox;
+		int cardsInUnfilledBox = tradingCards%cardsPerBox;
+		System.out.println(filledBoxes + " " + cardsInUnfilledBox);
 
         /*
         49. Susie’s father repaired the bookshelves in the reading room. If he has
         210 books to be distributed equally on the 10 shelves he repaired,
         how many books will each shelf contain?
         */
+		
+		int books = 210;
+		int numberOfShelves = 10;
+		int booksPerShelf = books / numberOfShelves;
+		System.out.println(booksPerShelf);
 
         /*
         50. Cristina baked 17 croissants. If she planned to serve this equally to
         her seven guests, how many will each have?
         */
+		
+		int croissantsBaked = 17;
+		int guests = 7;
+		int croissantsPerGuest = croissantsBaked/guests;
+		System.out.println(croissantsPerGuest);
 
         /*
             CHALLENGE PROBLEMS
@@ -433,6 +543,10 @@ public class Exercises {
         Hint: Calculate the hourly rate for each painter, combine them, and then divide the total walls in feet by the combined hourly rate of the painters.
         Challenge: How many days will it take the pair to paint 623 rooms assuming they work 8 hours a day?.
         */
+		
+		
+		
+		
 
         /*
         Create and assign variables to hold your first name, last name, and middle initial. Using concatenation,
