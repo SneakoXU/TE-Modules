e5d66eb7cb56c38e28811409098501821b4ee1ea
Niko Suntay <suntay.3@gmail.com>
2020-09-20 16:28:41 -0400

    DecimalToBinary completed

--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/DecimalToBinary.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/DecimalToBinary.java
@@ -5,28 +5,92 @@ import java.util.Scanner;
 public class DecimalToBinary {
 
 	public static void main(String[] args) {
-		System.out.println("Please enter in a series of decimal values (separated by spaces): ");
+		
+		//create input scanner, print prompt and store input
 		Scanner inputScanner = new Scanner(System.in);
+		printPrompt();
 		String userInput = inputScanner.nextLine();
+
+		//store input into a string array
+		String[] userArray = getUserInput(userInput);
 		
-		String[] binaryAnswer = getUserInput(userInput);
-		  
-  		for(int i = 0; i < binaryAnswer.length; i++) {
-  		  
-  			System.out.println( "Sqwak! " + angryAnswer[i] + "!"); 
-		}
+		//convert string[] to int[] in order to calculate
+		int[] intUserArray = changeToIntArray(userArray);
 		
-	
 		
+		
+		
+//		int[]intUserArray = new int[userArray.length]; 
+//  		for(int i = 0; i < userArray.length; i++) {
+//  			intUserArray[i] = Integer.parseInt(userArray[i]);	  
+//  		}
+		
+  		
+ 		binaryConvert(intUserArray);
+//  		binaryConvert(changeToIntArray(userArray));
+
+  		
+//			prints out items in int[]
+//  		for(int a = 0; a < intBinaryArray.length; a++) {
+//  		System.out.println(intBinaryArray[a]); 
+//		}
+//		
+//  		
+//	
 		inputScanner.close();
 	}
 
-		public static String[] getUserInput (String decInput) {
+		public static void printPrompt(){
+			System.out.println("Please enter in a series of decimal values (separated by spaces): ");
+		}
 	
-			
+	
+		public static String[] getUserInput (String decInput) {
 			return decInput.split(" ");
 			
 		}
 		
+		public static int[] changeToIntArray (String[] passedInArray){
+			int[] newIntArray = new int[passedInArray.length]; 
+			for(int i = 0; i < passedInArray.length; i++) {
+	  			newIntArray[i] = Integer.parseInt(passedInArray[i]);	  
+	  		}
+			return newIntArray;
+		}
 		
+		
+		//pass in array, calc binary and store it in array
+		public static void binaryConvert(int[] passedInArray) {
+			for(int n=0; n<passedInArray.length; n++) {
+			System.out.println(passedInArray[n] + " in binary is " + Integer.toBinaryString(passedInArray[n]));
+			
+			
+//			int[] newArray = new int[passedInArray.length];
+//			int counter = 0;
+//			
+//			for(int m=0; m<passedInArray.length; m++) {
+//				int binaryValue = passedInArray[m];
+//				newArray[counter] = binaryValue % 2;
+//				binaryValue = binaryValue / 2;
+//				counter++;
+//			}
+//			
+//			
+//			for(int a = 0; a < newArray.length; a++) {
+//	  		System.out.println(newArray[a]);	
+//				
+				
+				
+			}
+			
+		// int[] binaryNumber = new int[intBinaryArray.length]
+		// int counter = 0;	
+		// int binaryValue = 0;
+		// binaryNumber[i] = binaryValue % 2;
+		// binaryValue = binaryValue / 2;	
+		// counter++;	
+			
+			
+		}
+	
 }
