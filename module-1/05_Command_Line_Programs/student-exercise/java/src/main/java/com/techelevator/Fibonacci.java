0943e97a9d7f4f134649c7c37b9d9ffbe4938829
Niko Suntay <suntay.3@gmail.com>
2020-09-20 13:25:42 -0400

    Refactored TempConverter and starting Fibonacci

--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/Fibonacci.java
@@ -1,9 +1,45 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class Fibonacci {
 
 	public static void main(String[] args) {
+		System.out.println("Please enter the Fibonacci number: ");
+		Scanner in = new Scanner(System.in);
+		String fibString = in.nextLine();
+		Integer fibInt = Integer.parseInt(fibString);
+		
+		//boolean running = true;
+		int firstFib = 0;
+		int secondFib = 1;
+		
+		//while(running) {
+		//if(fibInt > 1) {
+		while(firstFib <= fibInt) {
+			
+			
+			
+			
+			}
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		in.close();
 
+	}
+	
+	public static int[] fibonacci(int fibNums) {
+		
+		
+		return int[];
+		
 	}
 
 }
--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/TempConvert.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/TempConvert.java
@@ -7,34 +7,58 @@ public class TempConvert {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("Please enter the temperature: ");
+		printPrompt();
 		Scanner in = new Scanner(System.in);
-		Double tempInputDouble = 0D;
-		String tempInput = tempInputDouble.toString();
-		tempInput = in.nextLine();
+		
+		String tempInput = in.nextLine();
+		Double tempInputDouble = Double.parseDouble(tempInput); 
 		
 		
-		System.out.println("Is the temperature in (C)elsius or (F)ahrenheit? ");
+		printTempChoice();
 		//Scanner inSelect = new Scanner(System.in);
 		String selection = in.nextLine();
 		
 		
 		if(selection.equalsIgnoreCase("C")) {
-			double fahrenheitTemp = 0;
-			fahrenheitTemp = tempInputDouble * 1.8 + 32;
-			System.out.println(tempInput + "C is " + fahrenheitTemp + "F.");
+//			double fahrenheitTemp = 0;
+//			fahrenheitTemp = tempInputDouble * 1.8 + 32;
+			System.out.println(celToF(tempInputDouble));
 		}
-		
-		
+//		
+//		
 		if(selection.equalsIgnoreCase("F")) {
-			double celsiusTemp = 0;
-			celsiusTemp = celsiusTemp * 1.8 + 32;
-			System.out.println(tempInput + "F is " + celsiusTemp + "C.");
+//			double celsiusTemp = 0;
+//			celsiusTemp = tempInputDouble * 1.8 + 32;
+			System.out.println(fahrenheitToC(tempInputDouble));
 		}
 		
 		in.close();
 		//inSelect.close();
 
 	}
+	
+	public static void printPrompt() {
+		System.out.println("Please enter the temperature: ");
+		
+	}
+	
+	public static void printTempChoice() {
+		System.out.println("Is the temperature in (C)elsius or (F)ahrenheit? ");
+	}
+	
+	public static String celToF(double celsiusInput) {
+		double fahrenheitTemp = 0;
+		fahrenheitTemp = celsiusInput * 1.8 + 32; //tempInpuTDouble
+		return (celsiusInput + "C is " + fahrenheitTemp + "F.");
 
-}
+	}
+	
+	public static String fahrenheitToC (double fahrenheitInput) {
+		double celsiusTemp = 0;
+		celsiusTemp = (fahrenheitInput - 32) / 1.8;
+		return (fahrenheitInput + "F is " + celsiusTemp + "C.");
+	
+		}
+	}
+	
+		
