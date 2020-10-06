eafa2318b21f85e35a765ab590765a9b78b50e35
Niko Suntay <suntay.3@gmail.com>
2020-10-05 22:13:10 -0400

    got second question and choices to print properly, need to add logic for user answers

--- module-1/16_FileIO_Reading_in/student-exercise/java/src/main/java/com/techelevator/QuizMaker.java
+++ module-1/16_FileIO_Reading_in/student-exercise/java/src/main/java/com/techelevator/QuizMaker.java
@@ -25,11 +25,13 @@ public class QuizMaker {
 		Scanner fileScanner;
 		try {
 			fileScanner = new Scanner(inputFile);
-			//while(fileScanner.hasNextLine()) {
+			while(fileScanner.hasNextLine()) {
 			
-				//prints entire line until the |
+				//stores each string until the |
 				String line = fileScanner.nextLine();
 				String[] lines = line.split("\\|");
+				
+				
 				
 				//stores first array before the | as the question
 				question = lines[0];
@@ -49,13 +51,44 @@ public class QuizMaker {
 				userChoice3 = userChoice3.replace("*", "");
 				System.out.println("3. " + userChoice3);
 				userChoice4 = lines[4];
+				userChoice3 = userChoice3.replace("*", "");
 				System.out.println("4. " + userChoice4);
+				
+				
+				/* NEED TO GET USER ANSWER FOR QUESTION 1 
+				 * AND CHECK IF CORRECT OR INCORRECT
+				 */
 
 				
+				//reads second line and separates at |
+				String lineTwo = fileScanner.nextLine();
+				String[] lines2 = lineTwo.split("\\|");
 				
+				//stores question
+				String question2 = lines2[0];
+				System.out.println(question2);
 				
+				/*
+				 * storing remaining arrays separated by | 
+				 * and removing asterisk
+				*/
+				userChoice1 = lines2[1];
+				userChoice1 = userChoice1.replace("*", "");
+				System.out.println("1. " + userChoice1);
+				userChoice2 = lines2[2];
+				userChoice2 = userChoice2.replace("*", "");
+				System.out.println("2. " + userChoice2);
+				userChoice3 = lines2[3];
+				userChoice3 = userChoice3.replace("*", "");
+				System.out.println("3. " + userChoice3);
+				userChoice4 = lines2[4];
+				userChoice3 = userChoice3.replace("*", "");
+				System.out.println("4. " + userChoice4);
 				
-				
+				/* NEED TO GET USER ANSWER FOR QUESTION 2 
+				 * AND CHECK IF CORRECT OR INCORRECT
+				 */		
+			}			
 			
 	}catch (FileNotFoundException e) {
 		e.printStackTrace();
